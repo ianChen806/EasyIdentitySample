6e7f133538b9be6c3581a1d8ba1db30b3cf42293
Ian <ian.chen@taiwantaxi.com.tw>
2020-04-08 12:16:28 +0800

    add services and middleware.

--- EasyIdentitySample/Controllers/MemberController.cs
+++ EasyIdentitySample/Controllers/MemberController.cs
@@ -1,21 +1,36 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using EasyIdentitySample.ViewModel;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyIdentitySample.Controllers
 {
     public class MemberController : Controller
     {
+        private readonly IHttpContextAccessor _accessor;
+
+        public MemberController(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Index(MemberViewModel request)
+        public async Task<IActionResult> Index(MemberViewModel request)
         {
             if (request.Account == "Test" && request.Password == "123")
             {
-                // TODO SignIn
+                var claims = new List<Claim>();
+                var claimsIdentity = new ClaimsIdentity(claims);
+                var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                await _accessor.HttpContext.SignInAsync(claimsPrincipal);
+                
                 return RedirectToAction("Index", "Home");
             }
             return View();
--- EasyIdentitySample/Startup.cs
+++ EasyIdentitySample/Startup.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +20,14 @@ namespace EasyIdentitySample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddAuthentication(options =>
+                    {
+                        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    })
+                    .AddCookie();
+
+            services.AddHttpContextAccessor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +49,7 @@ namespace EasyIdentitySample
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
